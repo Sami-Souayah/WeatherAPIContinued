b08b10f0de0fac944991168d14d12a41f1c57d4b
Sami Souayah <souays@bu.edu>
2024-12-12 00:29:18 -0500

    db setup

--- db/db_connection.py
+++ db/db_connection.py
@@ -1,12 +1,22 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import logging
+from weather_app.utils.logger import configure_logger
+
+logger = logging.getLogger(__name__)
+configure_logger(logger)
+
 import os
 
 load_dotenv()
 def get_database():
-    CONNECTION_STRING = os.getenv("CONNECTION_STRINGDB")
-    client = MongoClient(CONNECTION_STRING)
-    return client["Users"]
+    try:
+        logger.info("Connecting to database")
+        CONNECTION_STRING = os.getenv("CONNECTION_STRINGDB")
+        client = MongoClient(CONNECTION_STRING)
+        return client["Users"]
+    except:
+        logger.error("DB connection failed")
 
 if __name__=="__main__":
     dbname = get_database()
