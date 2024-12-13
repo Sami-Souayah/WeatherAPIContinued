cf8a9b04f5f77714b806ba606d23d43173fdbcf3
Sami Souayah <souays@bu.edu>
2024-12-13 16:38:43 -0500

    App changes

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'http://127.0.0.1:5000';
 
 function App() {
   const [username, setUsername] = useState('');
